01a3f381cca21497070137a05ed710bc5d02cef5
Wytze Gelderloos <wytzepakito@gmail.com>
2020-08-14 22:46:20 +0200

    refactoring to common crud interfaces. CLoses #11.

--- pet-clinic-data/src/main/java/pakito/wytze/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/pakito/wytze/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,11 @@ package pakito.wytze.sfgpetclinic.services;
 
 import pakito.wytze.sfgpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
 
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
