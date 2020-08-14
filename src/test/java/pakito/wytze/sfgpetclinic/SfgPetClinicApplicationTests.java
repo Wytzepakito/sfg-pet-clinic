e63f83d381fe1fe911b54c2a26e1d53cb6b92bb3
Wytze Gelderloos <wytzepakito@gmail.com>
2020-08-14 19:34:56 +0200

    First commit

---
+++ src/test/java/pakito/wytze/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package pakito.wytze.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
