244a31b09d87c53d8c109924b88277e762736052
Wytze Gelderloos <wytzepakito@gmail.com>
2020-08-17 14:52:45 +0200

    Added PetTypes bootstrap on startup. Closes #33.

--- pet-clinic-web/src/main/java/pakito/wytze/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pakito/wytze/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package pakito.wytze.sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pakito.wytze.sfgpetclinic.model.Owner;
+import pakito.wytze.sfgpetclinic.model.PetType;
 import pakito.wytze.sfgpetclinic.model.Vet;
 import pakito.wytze.sfgpetclinic.services.OwnerService;
+import pakito.wytze.sfgpetclinic.services.PetTypeService;
 import pakito.wytze.sfgpetclinic.services.VetService;
 
 @Component
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
