8a48d420df13d41c1fe5d186b20aa4ed1425ffa0
Wytze Gelderloos <wytzepakito@gmail.com>
2020-08-17 14:45:38 +0200

    Enhanced Owner. Closes #30.

---
+++ pet-clinic-data/src/main/java/pakito/wytze/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pakito.wytze.sfgpetclinic.services;
+
+import pakito.wytze.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/pakito/wytze/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package pakito.wytze.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pakito.wytze.sfgpetclinic.model.PetType;
+import pakito.wytze.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+
+}
